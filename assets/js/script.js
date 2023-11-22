c54fbab4f0899372e8c7aaa5dc3e1e65eec7b5ba
johannacarolinep <139339346+johannacarolinep@users.noreply.github.com>
2023-11-22 13:14:33 +0100

    Add function to create pantry and add pantry items to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,46 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", async function () {
     const scoreArea = document.getElementById("score-area");
     const questionArea = document.getElementById("question-area");
     const pantryArea = document.getElementById("pantry-area");
     const quitButton = document.getElementById("quit-button");
     const nextSubmitButton = document.getElementById("next-submit-button");
 
-    pullPantryData();
-    console.log(pullPantryData());
+    const pantryData = await pullPantryData();
+    console.log("Pantrydata:", pantryData);
+    createPantry(pantryData, pantryArea);
 })
 
+/**
+ * Pulls the pantry data from JSON
+ * Returns object pantryData
+ */
 async function pullPantryData() {
     const pantryRawData = await fetch("assets/json/pantry.json");
-    const pantryData = pantryRawData.json();
+    const pantryData = await pantryRawData.json();
     return pantryData;
+}
+
+/**
+ * Creates an array of all pantry item answers (ingredients)
+ * by concatenating the answers arrays of all pantry items and deduplicating.
+ * Builds the pantry divs in HTML.
+ */
+function createPantry(pantryData, pantryDiv) {
+    let masterPantryArray = [];
+
+    console.log(pantryData);
+
+    for (let i = 0; i < pantryData.pantry.length; i++) {
+        masterPantryArray = masterPantryArray.concat(pantryData.pantry[i].answers);
+        console.log(pantryData.pantry[i].answers);
+    }
+    console.log("Master pantry array:", masterPantryArray);
+    const pantryArray = [...new Set(masterPantryArray)];
+    console.log("pantryArray:", pantryArray);
+
+    for (item in pantryArray) {
+        const pantryItem = document.createElement("div");
+        pantryItem.innerHTML = pantryArray[item];
+        pantryDiv.appendChild(pantryItem);
+    }
 }
